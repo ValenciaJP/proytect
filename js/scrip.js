45860846b1130e3f69fa1d860621125c564bdfa5
ValenciaJP <3242995693jpvv@gmail.com>
2025-05-25 19:21:25 -0500

    footer
    termine pie de pagina

---
+++ js/scrip.js
@@ -0,0 +1,5 @@
+const bolsa = document.getElementById('bolsa');
+const elementos1 = document.getElementById ('lista-1');
+const lista = document.querySelector('lista-bolsa tbody');
+const vaciarbolsaBtn = document.getElementById('vaciar-bolsa');
+
